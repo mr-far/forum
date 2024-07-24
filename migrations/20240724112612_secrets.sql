79fcf54c25970fcda332f5fa12413b3810eb48e9
Mr.Far <88129219+mr-far@users.noreply.github.com>
2024-07-24 22:36:14 +0300

    Add new migration

---
+++ migrations/20240724112612_secrets.sql
@@ -0,0 +1,10 @@
+-- Add secrets table
+
+CREATE TABLE IF NOT EXISTS secrets (
+	id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	password_hash varchar NOT NULL,
+
+	secret1 BIGINT NOT NULL,
+	secret2 BIGINT NOT NULL,
+	secret3 BIGINT NOT NULL
+)
--- src/routes/categories.rs
+++ src/routes/categories.rs
@@ -8,17 +8,18 @@ use {
         routes::{Result, HttpError},
         models::{
             category::Category,
+            user::Permissions,
             requests::CreateCategoryPayload
         },
         utils::snowflake::Snowflake
     }
 };
-use crate::models::user::Permissions;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("categories")
             .route("", web::post().to(create_category))
+            .route("{category_id}", web::get().to(get_category))
     );
 }
 
@@ -31,6 +32,29 @@ async fn create_category(
         .map_err(|err| HttpError::Validation(err))?;
 
     let user = app.database.fetch_user(Snowflake(123))
+        .await
+        .ok_or(HttpError::UnknownUser)?;
+
+    if !user.clone().has_permission(Permissions::MANAGE_CATEGORIES) {
+        return Err(HttpError::MissingAccess)
+    }
+
+    let id = app.snowflake.lock().unwrap().build();
+    app.database.create_category(id, user.id, payload.into_inner())
+        .await
+        .map(|record| HttpResponse::Ok().json(Category::from(record, user)))
+        .map_err(|err| HttpError::Database(err))
+}
+
+async fn get_category(
+    payload: web::Json<CreateCategoryPayload>,
+    app: web::Data<AppData>,
+) -> Result<HttpResponse> {
+    payload
+        .validate()
+        .map_err(|err| HttpError::Validation(err))?;
+
+    let user = app.database.fetch_category(Snowflake(123))
         .await
         .ok_or(HttpError::UnknownUser)?;
 
