3a5749ab9c7ca30a8623397d426e5acb8c0d0ab4
Mr.Far <88129219+mr-far@users.noreply.github.com>
2024-07-25 22:36:36 +0300

    Add registration payload

--- src/models/requests.rs
+++ src/models/requests.rs
@@ -1,13 +1,23 @@
 use {
     serde::Deserialize,
-    validator::Validate
+    validator::Validate,
 };
 
 #[derive(Deserialize, Validate)]
 pub struct CreateCategoryPayload {
-    #[validate(length(min = 4, max = 128))]
+    #[validate(length(min = 4, max = 128, message="Title length must be between 4 and 128 chars"))]
     pub title: String,
-    #[validate(length(min = 16, max = 2048))]
+    #[validate(length(min = 16, max = 2048, message="Description length must be between 16 and 2048 chars"))]
     pub description: String,
     pub is_locked: bool
+}
+
+#[derive(Deserialize, Validate)]
+pub struct RegisterPayload {
+    #[validate(length(min = 12, message = "Too short password"))]
+    pub password: String,
+    #[validate(length(min = 2, max = 32, message = "Username length must be between 2 and 32 chars"))]
+    pub username: String,
+    #[validate(length(min = 2, max = 32, message = "Display name length must be between 2 and 32 chars"))]
+    pub display_name: String,
 }
