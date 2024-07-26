8ae6611359b8cc730d0f9c690e37021a54674215
Mr.Far <88129219+mr-far@users.noreply.github.com>
2024-07-26 23:44:51 +0300

    Changes in migrations

--- migrations/20240724112612_secrets.sql
+++ migrations/20240724112612_secrets.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS secrets (
 	id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 	password_hash varchar NOT NULL,
+	email varchar NOT NULL,
 
 	secret1 BIGINT NOT NULL,
 	secret2 BIGINT NOT NULL,
