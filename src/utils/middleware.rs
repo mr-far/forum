c435020736b428710b73baaa3db995eb7f077d30
Mr.Far <88129219+mr-far@users.noreply.github.com>
2024-08-26 23:05:24 +0300

    Add authorization middleware

---
+++ src/utils/middleware.rs
@@ -0,0 +1,23 @@
+use {
+    actix_web::{
+        Error, body::MessageBody, HttpMessage,
+        dev::{ServiceRequest, ServiceResponse}, web,
+        middleware::Next, http::header::AUTHORIZATION
+    },
+    crate::{
+        App, utils::authorization::extract_header,
+        models::UserCredentials
+    }
+};
+
+pub async fn authorization_middleware(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, Error> {
+    let token = extract_header(&req.request(), AUTHORIZATION)?;
+    let credentials = req.app_data::<web::Data<App>>().unwrap().database.fetch_credentials_by_token(token).await?;
+
+    req.extensions_mut().insert(UserCredentials(credentials.0, credentials.1));
+
+    next.call(req).await
+}
