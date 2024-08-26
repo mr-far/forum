e720d7e44edc510206f1954cfe1f05a36ed7f974
Mr.Far <88129219+mr-far@users.noreply.github.com>
2024-08-26 23:01:48 +0300

    Add `UserCredentials` & HEX-string letters

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,11 @@
+use {
+    serde::Serialize,
+    crate::models::{
+        session::Session,
+        user::User
+    }
+};
+
 pub mod error;
 pub mod database;
 pub mod category;
@@ -5,5 +13,12 @@ pub mod user;
 pub mod thread;
 pub mod message;
 pub mod requests;
-pub mod secret;
 pub mod gateway;
+pub mod session;
+
+#[derive(Serialize,Debug, Clone, PartialEq)]
+pub struct UserCredentials(pub Session, pub User);
+
+const _SESSION_ID_ALPHABET: [char; 16] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+];
